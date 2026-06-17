011545a216a50c262c1ed829bf067cc95ec39f11
Lucas Hiago de Azevedo Dantas <lucas.dantas@ifrn.edu.br>
2026-06-17 17:44:24 -0300

    Funcionalidade de busca

--- app/controllers/Pessoas.java
+++ app/controllers/Pessoas.java
@@ -18,9 +18,14 @@ public class Pessoas extends Controller {
 		renderTemplate("Pessoas/form.html", p);
 	}
 	
-	public static void listar() {
-		List<Pessoa> pessoas = Pessoa.findAll();
-		render(pessoas);
+	public static void listar(String termo) {
+		List<Pessoa> pessoas = null;
+		if (termo == null) {
+			pessoas = Pessoa.findAll();			
+		} else {
+			pessoas = Pessoa.find("lower(nome) like ?1 or lower(email) like ?1", "%"+termo.toLowerCase()+"%").fetch();
+		}
+		render(pessoas, termo);
 	}
 	
 	public static void detalhar(Long id) {
@@ -33,14 +38,14 @@ public class Pessoas extends Controller {
 		pessoa.email = pessoa.email.toLowerCase();
 		pessoa.save();
 		flash.success("Pessoa cadastrada com sucesso!");
-		listar();
+		listar(null);
 	}
 	
 	public static void remover(Long id) {
 		Pessoa qualquerNome = Pessoa.findById(id);
 		qualquerNome.delete();
 		flash.success("Pessoa removida com sucesso!");
-		listar();
+		listar(null);
 	}
 
 }
